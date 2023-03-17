3921713dc2842a443b43962f3b47bc7576eafd34
Arafat-Mahin310 <80392241+arafat-mahin310@users.noreply.github.com>
2023-03-18 01:34:11 +0600

    Old

--- OLD-BEST.py
+++ OLD-BEST.py
@@ -146,7 +146,7 @@ def Public():
 			data = requests.get("https://business.facebook.com/business_locations", headers = {"user-agent": "Mozilla/5.0 (Linux; Android 12.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36","referer": "https://www.facebook.com/","host": "business.facebook.com","origin": "https://business.facebook.com","upgrade-insecure-requests" : "1","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","cache-control": "max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8","content-type":"text/html; charset=utf-8"}, cookies = {"cookie":cookie}) 
 			find_token = re.search("(EAAG\w+)", data.text)
 			ken=open(".token.txt", "w").write(find_token.group(1))
-			print (" [] LOGIN SUCSESS")
+			print (" [] LOGIN SUCcESS")
 			login()
 		except Exception as e: 
 			os.system("rm -f .token.txt")
